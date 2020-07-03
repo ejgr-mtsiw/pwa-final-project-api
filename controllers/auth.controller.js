5b6824ac0b89e1003951f4dda7f0ac7df955617d
Eduardo Ribeiro <1600820@estudante.uab.pt>
2020-07-03 23:21:53 +0100

    On sucess authentication return user data

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,11 +3,20 @@ const messages = require('../config/messages/login');
 exports = module.exports = {};
 
 exports.signin = (req, res) => {
+    let userdata = {
+        id: req.user.id,
+        email: req.user.email,
+        name: req.user.name,
+        lastLogin: req.user.lastLogin,
+        role: req.user.role
+    };
+
     return res.status(messages.user.signinSucces.status)
-        .send(messages.user.signinSucces);
+        .send(userdata);
 };
 
 exports.signout = (req, res) => {
     req.session.destroy();
-    return res.redirect('/pwa/index.html?signout=ok');
+    return res.status(messages.user.logoutSuccess.status)
+        .send(messages.user.logoutSuccess);
 };
